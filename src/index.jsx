7abc30f14662a5300f09bae7dc9edc15d7032b31
Chris Harwood <cmharwood79@gmail.com>
2024-06-07 16:07:53 -0500

    added genres router

--- src/index.jsx
+++ src/index.jsx
@@ -2,15 +2,13 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { Provider } from 'react-redux';
 import './index.css';
-import store from './redux/store.js'
+import store from './redux/store.js';
 import App from './components/App/App.jsx';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 
 root.render(
-  <React.StrictMode>
-    <Provider store={store}>
-      <App />
-    </Provider>
-  </React.StrictMode>
+  <Provider store={store}>
+    <App />
+  </Provider>
 );
